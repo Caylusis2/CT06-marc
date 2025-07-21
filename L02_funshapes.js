04c6b6cde0bc32334159f7ab0c48320f498ce8d0
Caylusis2 <caylusis2@yahoo.com>
2025-07-21 09:58:15 +0000

    Jul 21, 2025, 9:58 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -9,7 +9,7 @@ function setup() {
 function draw() {
   // Challenge 1: An eye using ellipse() function
     fill(255,0,0);
-    Stroke(53,109,);
+    Stroke(53,200,255);
     ellipse(100,100,80,50);
     ellipse(200,100,80,50);
     
