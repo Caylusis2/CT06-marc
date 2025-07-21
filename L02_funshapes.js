e607831eb56dfafcd5a76ad44895e95e892d8a00
Caylusis2 <caylusis2@yahoo.com>
2025-07-21 09:49:40 +0000

    Jul 21, 2025, 9:49 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -26,6 +26,7 @@ function draw() {
     quad(120,30,180,30,180,70,120,70);
     quad(50,162,86,150,50,138,14,150);
     quad(120,150,180,130,180,170,120,170);
+    nofill();
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
