814fda2c252d4d856bc1c9abb99c8bffd4f7894e
Caylusis2 <caylusis2@yahoo.com>
2025-08-02 06:08:34 +0000

    Aug 2, 2025, 6:08 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,33 +5,33 @@ function setup() {
   }
   
 //let colourValue = 0;
-function draw() {
-  // Recap 1: Repeating Circles
-  // circle(75,200);
-  // circle(225,200);
-  // circle(375,200);
-  // circle(525,200);
+// function draw() {
+//   // Recap 1: Repeating Circles
+//   // circle(75,200);
+//   // circle(225,200);
+//   // circle(375,200);
+//   // circle(525,200);
 
-    // circle(75, 200, 100);
-    // circle(225, 200, 100);
-    // circle(375, 200, 100);
-    // circle(525, 200, 100);
+//   // circle(75, 200, 100);
+//   // circle(225, 200, 100);
+//   // circle(375, 200, 100);
+//   // circle(525, 200, 100);
 
 
-     for(let i=0; i<5; i++) {
-        // fill(0, 0, 255);
-        // fill(i*50);
-        // fill(colorValue%255);
-        // circle(spacing +i*spacing, 100, 40);
-        // colorValue +=1;
+//for(let i=0; i<5; i++) {
+//fill(0, 0, 255);
+//fill(i*50);
+//fill(colorValue%255);
+//circle(spacing +i*spacing, 100, 40);
+//colorValue +=1;
 
-        if(i%2==0) {
-            fill(0);
-        } else {
-            fill(255);
-        }
-        circle(spacing + i*spacing, 100, 40);
-    }
+//if(i%2==0) {
+//             fill(0);
+//         } else {
+//             fill(255);
+//         }
+//         circle(spacing + i*spacing, 100, 40);
+    // }
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
@@ -39,4 +39,5 @@ function draw() {
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
-}
+// }
+
