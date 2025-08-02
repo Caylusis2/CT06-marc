cc3905c07b0f684e39bd796e3a4c01fca481238a
Caylusis2 <caylusis2@yahoo.com>
2025-08-02 06:43:46 +0000

    Aug 2, 2025, 6:43 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -49,4 +49,9 @@ let numCircles = 5;
 let totalwidth = circleDiameter * numCircles;
 let startX = (width - totalwidth)/2 + circleDiameter/2;
 
+for(let i = 0; i < numCircles; i++)
+    let x = startX + i * circleDiameter;
+    ellipse (x, height/2,circleDiameter, circleDiameter);
+    circle(x, height/2,circleDiameter);
+  {}
 }
