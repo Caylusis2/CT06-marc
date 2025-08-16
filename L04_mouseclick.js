8643a88d931023dfe56ae0a1ae7490890841902a
Caylusis2 <caylusis2@yahoo.com>
2025-08-16 07:25:21 +0000

    Aug 16, 2025, 7:25 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,39 +1,39 @@
 // ---------------------------------- task 1
-// let shapeColor = "blue";
-// let x1,y1,x2 y2,x3,y3
+let shapeColor = "blue";
+let x1,y1,x2, y2,x3,y3
 
-// function setup(){
-//     createCanvas(400,400)
-//     background(200);
-// }
+function setup(){
+    createCanvas(400,400)
+    background(200);
+}
 
-// function draw(){
-//     fill(shapeColor);
-//     // ellipse(200,200,80,80)
-//     if(mousePressed){
-//     triangle(x1,y1,x2, y2,x3,y3);
-//     }
-// }
+function draw(){
+    fill(shapeColor);
+    // ellipse(200,200,80,80)
+    if(mousePressed){
+    triangle(x1,y1,x2, y2,x3,y3);
+    }
+}
 
-// // function mousePressed(){
-// //     shapeColor = "red"
-// // }
-// // function mouseReleased(){
-// //     shapeColor = "yellow"
-// // }
 // function mousePressed(){
-//     shapeColor = color(random(255), random(255), random(255))
-//     x1 = random(width);
-//     y1 = random(height);
-//     x2 = random(width);
-
-//     y2 = random(height);
-//     x3 = random(width);
-//     y3 = random(height);
+//     shapeColor = "red"
 // }
 // function mouseReleased(){
-//     shapeColor = color(random(255), random(255), random(255))
+//     shapeColor = "yellow"
 // }
+function mousePressed(){
+    shapeColor = color(random(255), random(255), random(255))
+    x1 = random(width);
+    y1 = random(height);
+    x2 = random(width);
+
+    y2 = random(height);
+    x3 = random(width);
+    y3 = random(height);
+}
+function mouseReleased(){
+    shapeColor = color(random(255), random(255), random(255))
+}
 
 // --------------------------------- task 2
 // let shapeColor ="blue";
