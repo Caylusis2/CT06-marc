4afb7e3ce4841211e787dfa068391a5b0c2db672
Caylusis2 <caylusis2@yahoo.com>
2025-08-16 06:47:37 +0000

    Aug 16, 2025, 6:47 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,6 +1,7 @@
 // write your codes here
 
 let shapeColor = "blue"
+let x1,y1,x2
 
 function setup(){
     createCanvas(400,400)
@@ -11,7 +12,7 @@ function draw(){
     fill(shapeColor);
     // ellipse(200,200,80,80)
     if(mousePressed){
-    triangle(x1,y1,x2,Width,height)
+    triangle(x1,y1,x2,width,height)
     }
 }
 
@@ -23,6 +24,9 @@ function draw(){
 // }
 function mousePressed(){
     shapeColor = color(random(255), random(255), random(255))
+    x1 = random(width)
+    y1 = random(height)
+    x2 = random(width)
 }
 function mouseReleased(){
     shapeColor = color(random(255), random(255), random(255))
