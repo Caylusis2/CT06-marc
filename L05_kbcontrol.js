a6f7b7c241021f2153cab7d49d763bf6578331d5
Caylusis2 <caylusis2@yahoo.com>
2025-08-23 07:32:20 +0000

    Aug 23, 2025, 7:32 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -30,5 +30,7 @@ function setup(){
 
 function draw(){
     background(220);
-    rect(width/2 - rectSize/2, height/2 - rectSize/2, rectSize, rectSize)
+    rect(width/2 - rectSize/2, height/2 - rectSize/2, rectSize, rectSize);
 }
+
+function keyPressed()
