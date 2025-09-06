988fc3faf8d4289a63ccb703f9a6b6821209c2a3
Caylusis2 <caylusis2@yahoo.com>
2025-09-06 07:23:50 +0000

    Sep 6, 2025, 7:23 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -75,8 +75,8 @@ function setup(){
 }
 function draw(){
     circle(width/2, height/2, 100);
-    if(keyCode === UP_ARROW){
-        fill(255,0,0);
+    if(keyCode === 87){
+        fill(255,255,0);
     }
     else if(keyCode === 40){
         fill(0,0,0);
