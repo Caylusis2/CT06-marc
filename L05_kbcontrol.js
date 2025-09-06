9a5c2a658c80b6a11d1d92009438d393bfb3f878
Caylusis2 <caylusis2@yahoo.com>
2025-09-06 06:31:10 +0000

    Sep 6, 2025, 6:31 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -53,8 +53,7 @@ function setup(){
 function draw() {
     background(220);
     if(showCircle){
-        Circle(width/2, height/2, 100);
-        
+        circle(width/2, height/2, 100);    
     }
 }
 
