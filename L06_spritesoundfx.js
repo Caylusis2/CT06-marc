7f2ed3da29244350de4805e204f3ae95dfe526a9
Caylusis2 <caylusis2@yahoo.com>
2025-09-13 07:19:19 +0000

    Sep 13, 2025, 7:19 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -6,3 +6,8 @@ function preload(){
     staticImage = loadImage('assets/pico-a.png');
 }
 
+function setup(){
+    createCanvas(200,200);
+    background('lightblue');
+    image(staticImage,0,0,100,100);
+}
