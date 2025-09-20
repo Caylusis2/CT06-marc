675477b172b90b403387ca9daeedd2e8f219e5b0
Caylusis2 <caylusis2@yahoo.com>
2025-09-20 07:08:58 +0000

    Sep 20, 2025, 7:08 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -6,6 +6,10 @@ let y = 50;
 let w = 100;
 let h = 100;
 
+function preload(){
+    staticImage = loadImage('assets/pico-a.png');
+}
+
 function setup(){
     createCanvas(200,200);
     background('lightblue');
@@ -13,7 +17,19 @@ function setup(){
 
 function draw(){
     background('lightblue');
-    image(staticImage,x,y)
+    image(staticImage,x,y,w,h);
+    if (keycode === 37){
+        x-=5;
+    }
+    if (keycode === 38){
+    y-=5;
+    }
+    if (keycode === 39){
+    x+=5;
+    }
+    if (keycode === 40){
+    y+=5;
+    }
 }
 
 function keyPressed(){
