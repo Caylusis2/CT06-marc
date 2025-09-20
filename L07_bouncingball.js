07459fe5a06f41f31f6e84199a05672d25c707b0
Caylusis2 <caylusis2@yahoo.com>
2025-09-20 06:48:18 +0000

    Sep 20, 2025, 6:48 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,5 +1,10 @@
 // write your codes here
 
+function setup(){
+    createCanvas(200,200);
+    image(staticImage,0,0,100,100);
+}
+
 let staticImage,soundEffect;
 function preload(){
     staticImage = loadImage('assets/pico-a.png');
