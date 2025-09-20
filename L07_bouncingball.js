d119029a70d92063ab226d855c775ca6d3af96f3
Caylusis2 <caylusis2@yahoo.com>
2025-09-20 07:17:38 +0000

    Sep 20, 2025, 7:17 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,45 +1,45 @@
 // write your codes here
 
-let staticImage,soundEffect;
-let x = 50;
-let y = 50;
-let w = 100;
-let h = 100;
+// let staticImage,soundEffect;
+// let x = 50;
+// let y = 50;
+// let w = 100;
+// let h = 100;
 
-function preload(){
-    staticImage = loadImage('assets/pico-a.png');
-}
+// function preload(){
+//     staticImage = loadImage('assets/pico-a.png');
+// }
 
-function setup(){
-    createCanvas(200,200);
-    background('lightblue');
-}
+// function setup(){
+//     createCanvas(200,200);
+//     background('lightblue');
+// }
 
-function draw(){
-    background('lightblue');
-    image(staticImage,x,y,w,h);
-    if (keycode === 37){
-        x-=5;
-    }
-    if (keycode === 38){
-    y-=5;
-    }
-    if (keycode === 39){
-    x+=5;
-    }
-    if (keycode === 40){
-    y+=5;
-    }
-}
+// function draw(){
+//     background('lightblue');
+//     image(staticImage,x,y,w,h);
+//     if (keycode === 37){
+//         x-=5;
+//     }
+//     if (keycode === 38){
+//     y-=5;
+//     }
+//     if (keycode === 39){
+//     x+=5;
+//     }
+//     if (keycode === 40){
+//     y+=5;
+//     }
+// }
 
-function keyPressed(){
-    if(keyCode === 32){
-       w = 50;
-    }
-}
+// function keyPressed(){
+//     if(keyCode === 32){
+//        w = 50;
+//     }
+// }
 
-function keyReleased(){
-    w = 100;
+// function keyReleased(){
+//     w = 100;
 
-}
+// }
 
