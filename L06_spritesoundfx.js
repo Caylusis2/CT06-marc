57cfc3e3de8049fd02a0b09fc95e5385fb163c95
Caylusis2 <caylusis2@yahoo.com>
2025-09-20 06:24:44 +0000

    Sep 20, 2025, 6:24 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -4,7 +4,7 @@ let staticImage,soundEffect
 
 function preload(){
     staticImage = loadImage('assets/pico-a.png');
-    soundeffect = loadSound('')
+    soundeffect = loadSound('assets/pop.mp3')
 }
 
 function setup(){
