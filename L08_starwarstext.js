1957fab668d18cb43d730fced7c678dc37227771
Caylusis2 <caylusis2@yahoo.com>
2025-09-27 07:09:04 +0000

    Sep 27, 2025, 7:09 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,18 +1,19 @@
 // write your codes here
 
-let X = 200;
-let Y = 300;
-let SpeedX = 2;
-function setup(){
-    createCanvas(600,400);
-}
+// let X = 200;
+// let Y = 300;
+// let SpeedX = 2;
+// function setup(){
+//     createCanvas(600,400);
+// }
 
-function draw(){
-    background(220);
-    X += SpeedX;
-    if (X + 50 <= 0 || X + 50 >= width) {
-        fill(random(255), random(255), random(255));
-        SpeedX *= -1;
-    }
-    rect(X, Y, 50, 50);
-}
+// function draw(){
+//     background(220);
+//     X += SpeedX;
+//     if (X + 50 <= 0 || X + 50 >= width) {
+//         fill(random(255), random(255), random(255));
+//         SpeedX *= -1;
+//     }
+//     rect(X, Y, 50, 50);
+// }
+
