68aa1edb40f4f3c58ff8baf11b0443c6ce68eb7b
Caylusis2 <caylusis2@yahoo.com>
2025-10-04 06:32:15 +0000

    Oct 4, 2025, 6:32 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -12,7 +12,7 @@ function draw(){
     background(220);
     fill(0);
     for(let i = 0; i < favoriteFoods.length; i++){
-        text((i + 1) + "." + favoriteFoods[i],300,80 + i*30);
+        text((i + 1) + "." + favoriteFoods[i],300,80 + i*30) + yPos;
     }
     yPos -=0.6;
 }
