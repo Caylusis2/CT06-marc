c0edf96b43999bef604b149074ac0f4077df0872
Caylusis2 <caylusis2@yahoo.com>
2025-10-04 06:16:20 +0000

    Oct 4, 2025, 6:16 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -5,7 +5,7 @@ function setup(){
     createCanvas(400,400);
     background(220);
     textSize(34);
-    textAlign(CENTER,CENTER);
+    textAlign(RIGHT,CENTER);
     text(favoriteFoods, 100,30);
 
 }
