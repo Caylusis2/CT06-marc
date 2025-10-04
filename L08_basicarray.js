e2934bd60c2f2d588a587c248dddce0e5123be29
Caylusis2 <caylusis2@yahoo.com>
2025-10-04 06:11:01 +0000

    Oct 4, 2025, 6:11 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,3 +1,10 @@
 // write your codes here
 
 let favoriteFoods = ["pizza", "Burger", "sushi", "Tacos", "Pasta"]
+function setup(){
+    createCanvas(400,400);
+    background(220);
+    textSize(40);
+    textAlign(RIGHT,TOP);
+
+}
