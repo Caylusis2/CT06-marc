802eed07da1d60fa280eb772a493a4e771675283
Caylusis2 <caylusis2@yahoo.com>
2025-10-11 06:43:51 +0000

    Oct 11, 2025, 6:43 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -5,3 +5,16 @@ function setup(){
     textAlign(CENTER,CENTER);
 }
 
+function draw(){
+    background(225);
+    let h = hour();
+    let m = minute();
+    let s = second();
+
+    let stringTime = (h, 2) + ":" + nf(m, 2) + ":" + nf(s, 2);
+    fill(0);
+    textSize(46);
+    text(stringTime, width/2, height/2);
+
+
+}
