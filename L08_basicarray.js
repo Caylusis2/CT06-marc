f1e6b5020cc96c1aff16c1c74797551508ffffbf
Caylusis2 <caylusis2@yahoo.com>
2025-10-11 06:13:12 +0000

    Oct 11, 2025, 6:13 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -18,4 +18,47 @@ function draw(){
     yPos -=0.6;
 }
 
-{}
+];
+let yPos;
+function setup(){
+    createCanvas(600,400);
+    yPos = height;
+}
+
+function draw(){
+    background(0);
+    fill(255,255,0);
+    textSize(24);
+    textAlign(CENTER,CENTER);
+    if (audioStarted){
+        push();
+        translate(width/2, yPos);
+        scale(1,3);
+        for(let i = 0; i<storyText.length; i++){
+            text(storyText[i], 0, i* 20);
+        }
+        pop();
+
+        yPos -=0.6;
+        console.log(yPos);
+        if(yPos < - storyText.length *60){
+            yPos = height;
+        }
+    }
+    else{
+        text("Click to start the show", width/2, height/2);
+    }
+}
+function mousePressed(){
+    if(!audioStarted){
+        userStartAudio();
+        bg.Music.loop();
+        loop();
+        audioStarted = true;
+    }
+    else{
+        audioStarted = false;
+        bgMusic.stop();
+        noLoop();
+    }
+}
--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,7 @@
 // write your codes here
+
+function setup(){
+    createCanvas(400,500);
+    background(220);
+}
+
