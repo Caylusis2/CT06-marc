ca248da125326c855117945c84976796f85f82f7
Caylusis2 <caylusis2@yahoo.com>
2025-10-18 07:18:11 +0000

    Oct 18, 2025, 7:18 AM

--- L10_name.js
+++ L10_name.js
@@ -38,8 +38,9 @@ let userText = "Your Text Here";
 
 function setup(){
     createCanvas(600,400);
-}
 
-inputText = createInput();
+
+ inputText = createInput();
 inputText.position(200, height -80);
 inputText.input(updateText);
+}
