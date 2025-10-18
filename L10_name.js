b8028929e9fcca10b7f3c2d27b2f8939e4b18d9e
Caylusis2 <caylusis2@yahoo.com>
2025-10-18 07:10:56 +0000

    Oct 18, 2025, 7:10 AM

--- L10_name.js
+++ L10_name.js
@@ -1,34 +1,35 @@
 // write your codes here
-let countdown = 5;
-let timerStarted = false;
-function setup(){
-    createCanvas(400,400);
-    textAlign(CENTER,CENTER);
-    bgcolor = color(220);
-}
+// let countdown = 5;
+// let timerStarted = false;
+// function setup(){
+//     createCanvas(400,400);
+//     textAlign(CENTER,CENTER);
+//     bgcolor = color(220);
+// }
 
-function draw(){
-    background(bgcolor);
-    text(countdown, width/2, height/2 +20);
+// function draw(){
+//     background(bgcolor);
+//     text(countdown, width/2, height/2 +20);
 
-console.log(countdown);
-}
+// console.log(countdown);
+// }
 
-function mousePressed(){
-    if(!timerStarted){
-        timerStarted = true;
-        interval = setInterval(updateCountdown, 1000);
-    }
-}
+// function mousePressed(){
+//     if(!timerStarted){
+//         timerStarted = true;
+//         interval = setInterval(updateCountdown, 1000);
+//     }
+// }
 
-function updateCountdown(){
-    if(countdown > 0){
-        countdown-=1;
-        bgcolor = color(random(225), random(225), random(225));
-        console.log(countdown);
-    }else{
-        clearInterval(interval);
-        countdown = 5;
-        timerStarted = false;
-    }
-}
+// function updateCountdown(){
+//     if(countdown > 0){
+//         countdown-=1;
+//         bgcolor = color(random(225), random(225), random(225));
+//         console.log(countdown);
+//     }else{
+//         clearInterval(interval);
+//         countdown = 5;
+//         timerStarted = false;
+//     }
+// }
+
