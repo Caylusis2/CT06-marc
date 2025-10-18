de78c97df915ac29133bec0f5934d767aa167c4c
Caylusis2 <caylusis2@yahoo.com>
2025-10-18 06:11:54 +0000

    Oct 18, 2025, 6:11 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,40 +1,40 @@
 // write your codes here
-let countdown = 5;
-let interval;
-let bgcolour;
-function setup(){
-    createCanvas(600,400);
-    textAlign(CENTER,CENTER);
-}
+// let countdown = 5;
+// let interval;
+// let bgcolour;
+// function setup(){
+//     createCanvas(600,400);
+//     textAlign(CENTER,CENTER);
+// }
 
-function draw(){
-    background(bgcolour);
-    let h = hour();
-    let m = minute();
-    let s = second();
+// function draw(){
+//     background(bgcolour);
+//     let h = hour();
+//     let m = minute();
+//     let s = second();
 
-    let stringTime = (h, 2) + ":" + nf(m, 2) + ":" + nf(s, 2);
-    fill(0);
-    textSize(46);
+//     let stringTime = (h, 2) + ":" + nf(m, 2) + ":" + nf(s, 2);
+//     fill(0);
+//     textSize(46);
 
-    if( countdown == 0 ){
-        bgcolour = colour(random(255), random(255), random(255));
-    }
+//     if( countdown == 0 ){
+//         bgcolour = colour(random(255), random(255), random(255));
+//     }
 
-    text(stringTime, width/2, height/2 - 50);
+//     text(stringTime, width/2, height/2 - 50);
 
-    text(countdown, wdith/2, height/2 + 20);
-}
+//     text(countdown, wdith/2, height/2 + 20);
+// }
 
-function mousePressed(){
-    interval = setInterval(updateCountdown, 1000);
-}
+// function mousePressed(){
+//     interval = setInterval(updateCountdown, 1000);
+// }
 
-function updateCountdown(){
-    if(countdown > 0){
-        countdown-=1;
-    }else {
-        clearInterval(interval)
-    }
-}
+// function updateCountdown(){
+//     if(countdown > 0){
+//         countdown-=1;
+//     }else {
+//         clearInterval(interval)
+//     }
+// }
 
