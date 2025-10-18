6486aa075894e20307b795cf688dd7ae9439c688
Caylusis2 <caylusis2@yahoo.com>
2025-10-18 06:25:13 +0000

    Oct 18, 2025, 6:25 AM

--- L10_name.js
+++ L10_name.js
@@ -8,3 +8,11 @@ function draw(){
     background(bgcolour);
     Text(countdown, width/2, height/2 + 20);
 }
+
+function mousePressed(){
+    if(!timerStarted){
+        timerStarted = true;
+        interval = setInterval(updateCountdown, 1000);
+    }
+}
+
