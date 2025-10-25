a96ce0db2b7ee542a93df7ea117d781373b8c6ff
Caylusis2 <caylusis2@yahoo.com>
2025-10-25 07:20:44 +0000

    Oct 25, 2025, 7:20 AM

--- L10_name.js
+++ L10_name.js
@@ -57,3 +57,19 @@
 // function updateText(){
 //     userText = this.value();
 // }
+
+let favoriteFoods = ["Pizza", "Burger", "Sushi"];
+let pos;
+
+function setup(){
+    createCanvas(400, 200);
+    pos = 50;
+    textSize(24);
+}
+
+function draw(){
+    background(220);
+    for (let i = 0; i < favoriteFoods.length; i++){
+        text(favoriteFoods[i], pos + i * 100, height/2);
+    }
+}
