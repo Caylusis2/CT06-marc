3b5ca4aa631bda0ab1f0056284c0174e740ddc41
Caylusis2 <caylusis2@yahoo.com>
2025-11-01 06:49:07 +0000

    Nov 1, 2025, 6:49 AM

--- L10_name.js
+++ L10_name.js
@@ -127,7 +127,7 @@ function draw(){
 
     text("Pick a background color:", 100, height -32);
     text("Name:", 100, height -110);
-    text("Age:", 100)
+    text("Age:", 100, height -80)
 }
 function updateText(){
     userText = this.value();
