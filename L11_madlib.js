dae34db899d1ce8a3a93a917fefee35e686f3c00
Caylusis2 <caylusis2@yahoo.com>
2025-11-01 07:17:46 +0000

    Nov 1, 2025, 7:17 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -14,6 +14,18 @@ function setup(){
     inputField = createInput("Enter text here ");
     inputField.position(55, 20);
 
+    inputField = createInput("Enter text here ");
+    inputField.position(55, 20);
+
+    inputField = createInput("Enter text here ");
+    inputField.position(55, 20);
+
+    inputField = createInput("Enter text here ");
+    inputField.position(55, 20);
+
+
+
+
     submitButton = createButton("show");
     submitButton.position(inputField.X + inputField.width + 10, inputField.y);
     submitButton.mousePressed(displayInput);
