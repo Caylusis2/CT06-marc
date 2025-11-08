c43bceb52d64b34c6da810058453f32a95c8792d
Caylusis2 <caylusis2@yahoo.com>
2025-11-08 05:56:36 +0000

    Nov 8, 2025, 5:56 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,46 +1,47 @@
 // write your codes here
-let NounField;
-let VerbField;
-let AdjField;
-let AdverbField;
-let placeField;
-let submitButton;
-let outputArea;
+// let NounField;
+// let VerbField;
+// let AdjField;
+// let AdverbField;
+// let placeField;
+// let submitButton;
+// let outputArea;
 
-function setup(){
-    createCanvas(600,600)
-    background(220);
-
-    NounField = createInput("Noun");
-    NounField.position(55, 20);
-
-    VerbField = createInput("Verb");
-    VerbField.position(55, 60);
-
-    AdjField = createInput("Adjective");
-    AdjField.position(55, 100);
-
-    AdverbField = createInput("Adverb");
-    AdverbField.position(55, 140);
-
-    placeField = createInput("place");
-    placeField.position(55, 180);
-
-    submitButton = createButton("show");
-    submitButton.position(55,220);
-    submitButton.mousePressed(displayInput);
-
-}
-function displayInput(){
-    console.log(NounField.value());
-    console.log(VerbField.value());
-    console.log(AdjField.value());
-    console.log(AdverbField.value());
-    console.log(placeField.value());
+// function setup(){
+//     createCanvas(600,600)
 //     background(220);
-//     const inputValue = inputField.value();
-//     textSize(64);
-//     textAlign(CENTER,CENTER);
-//     text(inputValue, 200, 100);
+
+//     NounField = createInput("Noun");
+//     NounField.position(55, 20);
+
+//     VerbField = createInput("Verb");
+//     VerbField.position(55, 60);
+
+//     AdjField = createInput("Adjective");
+//     AdjField.position(55, 100);
+
+//     AdverbField = createInput("Adverb");
+//     AdverbField.position(55, 140);
+
+//     placeField = createInput("place");
+//     placeField.position(55, 180);
+
+//     submitButton = createButton("show");
+//     submitButton.position(55,220);
+//     submitButton.mousePressed(displayInput);
+
 // }
-}
+// function displayInput(){
+//     console.log(NounField.value());
+//     console.log(VerbField.value());
+//     console.log(AdjField.value());
+//     console.log(AdverbField.value());
+//     console.log(placeField.value());
+// //     background(220);
+// //     const inputValue = inputField.value();
+// //     textSize(64);
+// //     textAlign(CENTER,CENTER);
+// //     text(inputValue, 200, 100);
+// // }
+// }
+
